652812dea620e3bd04f291248330e8cf7dede3a0
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-09-16 11:53:39 +0300

    Initial Commit

---
+++ Mrbet/MrbetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MrbetApp.swift
+//  Mrbet
+//
+//  Created by Роман on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MrbetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
